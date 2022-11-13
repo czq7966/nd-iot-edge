2afa53e92e0d8419203552c81e9378d8e464ae9e
陈志谦 <761208@nd.com>
2022-11-13 18:37:50 +0800

    add node-red-nd-device-shadow v0.1.0

---
+++ update_shadows.cmd
@@ -0,0 +1,30 @@
+# dataio
+cd E:/data/nd-iot-edge/data/dataio/nd
+npm i node-red-nd-device-shadow@0.1.0
+
+# dispatcher
+cd E:/data/nd-iot-edge/data/dispatcher/nd
+npm i node-red-nd-device-shadow@0.1.0
+
+# edge
+cd E:/data/nd-iot-edge/data/edge/nd
+npm i node-red-nd-device-shadow@0.1.0
+
+# log
+cd E:/data/nd-iot-edge/data/log/nd
+npm i node-red-nd-device-shadow@0.1.0
+
+
+# platform bfy
+cd E:/data/nd-iot-edge/data/platform/bfy
+npm i node-red-nd-device-shadow@0.1.0
+
+# platform ndv1
+cd E:/data/nd-iot-edge/data/platform/ndv1
+npm i node-red-nd-device-shadow@0.1.0
+
+# platform ndv2
+cd E:/data/nd-iot-edge/data/platform/ndv2
+npm i node-red-nd-device-shadow@0.1.0
+
+
