5bf9492ae46d0160da6c1e0967cedc476d29546b
陈志谦 <761208@nd.com>
2022-11-14 19:50:17 +0800

    update node-red-nd-device-shadow v0.1.1

--- update_shadows.cmd
+++ update_shadows.cmd
@@ -1,30 +1,33 @@
 # dataio
 cd E:/data/nd-iot-edge/data/dataio/nd
-npm i node-red-nd-device-shadow@0.1.0
+npm i node-red-nd-device-shadow@0.1.1
 
 # dispatcher
 cd E:/data/nd-iot-edge/data/dispatcher/nd
-npm i node-red-nd-device-shadow@0.1.0
+npm i node-red-nd-device-shadow@0.1.1
 
 # edge
 cd E:/data/nd-iot-edge/data/edge/nd
-npm i node-red-nd-device-shadow@0.1.0
+npm i node-red-nd-device-shadow@0.1.1
 
 # log
 cd E:/data/nd-iot-edge/data/log/nd
-npm i node-red-nd-device-shadow@0.1.0
+npm i node-red-nd-device-shadow@0.1.1
 
 
 # platform bfy
 cd E:/data/nd-iot-edge/data/platform/bfy
-npm i node-red-nd-device-shadow@0.1.0
+npm i node-red-nd-device-shadow@0.1.1
 
 # platform ndv1
 cd E:/data/nd-iot-edge/data/platform/ndv1
-npm i node-red-nd-device-shadow@0.1.0
+npm i node-red-nd-device-shadow@0.1.1
 
 # platform ndv2
 cd E:/data/nd-iot-edge/data/platform/ndv2
-npm i node-red-nd-device-shadow@0.1.0
+npm i node-red-nd-device-shadow@0.1.1
 
+cd E:/data/nd-iot-edge
 
+# cd E:\data\nd-iot-edge\data\edge\nd\node_modules
+# mklink /D node-red-nd-device-shadow E:\data\ndiot-device-shadow\dist\node-red
