799832815fd0a12fd9402f651f32c18cfa1dbcb4
陈志谦 <761208@nd.com>
2022-11-18 21:34:14 +0800

    fix subscribe

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let mqtt = require("mqtt");
+let subscribe = mqtt.MqttClient.prototype.subscribe;
+mqtt.MqttClient.prototype.subscribe  = function(topic, opts, callback) {
+    if (opts)   
+        opts.qos = 0;
+    return subscribe.call(this, topic, opts, callback);
+} ;
+
 process.env.TZ = "Asia/Shanghai"
 require("./src/index.js")
