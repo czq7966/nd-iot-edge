9d645cccc99acfb6180200d129999d34b13eb8f4
陈志谦 <761208@nd.com>
2022-12-03 22:14:41 +0800

    update ndv1/ndv2 flows and device-shadow comp to v0.1.2

--- update_shadows.cmd
+++ update_shadows.cmd
@@ -1,31 +1,31 @@
 # dataio
 cd E:/data/nd-iot-edge/data/dataio/nd
-npm i node-red-nd-device-shadow@0.1.1
+npm i node-red-nd-device-shadow@0.1.2
 
 # dispatcher
 cd E:/data/nd-iot-edge/data/dispatcher/nd
-npm i node-red-nd-device-shadow@0.1.1
+npm i node-red-nd-device-shadow@0.1.2
 
 # edge
 cd E:/data/nd-iot-edge/data/edge/nd
-npm i node-red-nd-device-shadow@0.1.1
+npm i node-red-nd-device-shadow@0.1.2
 
 # log
 cd E:/data/nd-iot-edge/data/log/nd
-npm i node-red-nd-device-shadow@0.1.1
+npm i node-red-nd-device-shadow@0.1.2
 
 
 # platform bfy
 cd E:/data/nd-iot-edge/data/platform/bfy
-npm i node-red-nd-device-shadow@0.1.1
+npm i node-red-nd-device-shadow@0.1.2
 
 # platform ndv1
 cd E:/data/nd-iot-edge/data/platform/ndv1
-npm i node-red-nd-device-shadow@0.1.1
+npm i node-red-nd-device-shadow@0.1.2
 
 # platform ndv2
 cd E:/data/nd-iot-edge/data/platform/ndv2
-npm i node-red-nd-device-shadow@0.1.1
+npm i node-red-nd-device-shadow@0.1.2
 
 cd E:/data/nd-iot-edge
 
