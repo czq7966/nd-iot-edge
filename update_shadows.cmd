c497b0bd53727ac73bbfab8564859743f1ac1aab
陈志谦 <761208@nd.com>
2023-02-26 20:31:12 +0800

    update node-red-nd-device-shadow@0.1.5

--- update_shadows.cmd
+++ update_shadows.cmd
@@ -1,31 +1,31 @@
 echo "dataio"
 cd E:/data/nd-iot-edge/data/dataio/nd
-npm i node-red-nd-device-shadow@0.1.3
+npm i node-red-nd-device-shadow@0.1.5
 
 echo "dispatcher"
 cd E:/data/nd-iot-edge/data/dispatcher/nd
-npm i node-red-nd-device-shadow@0.1.3
+npm i node-red-nd-device-shadow@0.1.5
 
 echo "edge"
 cd E:/data/nd-iot-edge/data/edge/nd
-npm i node-red-nd-device-shadow@0.1.3
+npm i node-red-nd-device-shadow@0.1.5
 
 echo "log"
 cd E:/data/nd-iot-edge/data/log/nd
-npm i node-red-nd-device-shadow@0.1.3
+npm i node-red-nd-device-shadow@0.1.5
 
 
 echo "platform bfy"
 cd E:/data/nd-iot-edge/data/platform/bfy
-npm i node-red-nd-device-shadow@0.1.3
+npm i node-red-nd-device-shadow@0.1.5
 
 echo "platform ndv1"
 cd E:/data/nd-iot-edge/data/platform/ndv1
-npm i node-red-nd-device-shadow@0.1.3
+npm i node-red-nd-device-shadow@0.1.5
 
 echo "platform ndv2"
 cd E:/data/nd-iot-edge/data/platform/ndv2
-npm i node-red-nd-device-shadow@0.1.3
+npm i node-red-nd-device-shadow@0.1.5
 
 cd E:/data/nd-iot-edge
 
